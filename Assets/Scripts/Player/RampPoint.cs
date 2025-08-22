470220216081b851466129c091457deb9aa049e7
ayato0913a <ayato0913a@gmail.com>
2025-08-23 08:40:04 +0900

    [ add ] ランプ移動で使う用の設定スクリプトを追加

---
+++ Assets/Scripts/Player/RampPoint.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Stage
+{
+    /// <summary>
+    /// ランプ1つ分の情報（始点・頂点・終点）
+    /// </summary>
+    public class RampPoint : MonoBehaviour
+    {
+        public Transform start;
+        public Transform peak;
+        public Transform end;
+    }
+}
