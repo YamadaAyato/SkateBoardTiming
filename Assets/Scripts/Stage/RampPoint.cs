e1bf9878e50b9784a8d756f430541eb6c559f49f
ayato0913a <ayato0913a@gmail.com>
2025-09-09 07:02:57 +0900

    [ update ] Spline対応用に

--- Assets/Scripts/Stage/RampPoint.cs
+++ Assets/Scripts/Stage/RampPoint.cs
@@ -1,14 +1,13 @@
-﻿using UnityEngine;
+﻿using Unity.Cinemachine;
+using UnityEngine;
 
 namespace Stage
 {
     /// <summary>
-    /// ランプ1つ分の情報（始点・頂点・終点）
+    ///         ランプ1つ分の情報 
     /// </summary>
     public class RampPoint : MonoBehaviour
     {
-        public Transform start;
-        public Transform peak;
-        public Transform end;
+        public CinemachineSplineCart splineCartPrehub;
     }
 }
