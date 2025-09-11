79791e0db58f0abfa664623c97256c8f9c567954
ayato0913a <ayato0913a@gmail.com>
2025-09-11 19:54:08 +0900

    [ add ] Sceme管理をするクラスを追加

---
+++ Assets/Scripts/System/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+///         SceneLoadを制御するクラス
+/// </summary>
+public class SceneLoader : MonoBehaviour
+{
+    /// <summary>
+    ///         シーンを同期ロード
+    /// </summary>
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    /// <summary>
+    ///         シーンを非同期ロード
+    /// </summary>
+    public void LoadSceneAsync(string sceneName)
+    {
+        StartCoroutine(LoadAsync(sceneName));
+    }
+
+    private System.Collections.IEnumerator LoadAsync(string sceneName)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+}
