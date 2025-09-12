03b8b4a8aa49027f0ac4d9ac03f7243218f10585
ayato0913a <ayato0913a@gmail.com>
2025-09-13 06:40:56 +0900

    [ add ] 仮づくり

--- Assets/Scripts/Player/JumpTimingGameManager.cs
+++ Assets/Scripts/Player/JumpTimingGameManager.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class JumpTimingGameManager : MonoBehaviour
 {
-    [SerializeField] private float _noteDiameter = 1.5f;
+
 
     public void StartTimingGame(float jumpDuration)
     {
-        int noteCount = Mathf.CeilToInt(jumpDuration * _noteDiameter);
-        Debug.Log($"ジャンプ開始{jumpDuration:F1}秒　→　ノーツ{noteCount}個");
 
 
+    }
+
+    private void Update()
+    {
+        
     }
 }
---
+++ Assets/Scripts/TimingGameSystem/NoteJudgeController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+/// <summary>
+///         プレイヤー側のノーツ判定をつかさどるクラス
+/// </summary>
+public class NoteJudgeController : MonoBehaviour
+{
+
+}
