9b76f38a24f0cce4943a64210949f7eceb48f8c3
ayato0913a <ayato0913a@gmail.com>
2025-09-16 09:52:41 +0900

    [ add ] 加速用の板のスクリプトを追加

---
+++ Assets/Scripts/BarrageEvent/AcceleratorPad.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AcceleratorPad : MonoBehaviour
+{
+    [SerializeField] private BarrageGameFlow _gameFlow;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Playerタグのオブジェクトに当たったときだけ反応
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("加速板に乗った！ジャンプ開始");
+
+            _gameFlow.StartFinalJump();
+            Destroy(this.gameObject);
+        }
+    }
+}
