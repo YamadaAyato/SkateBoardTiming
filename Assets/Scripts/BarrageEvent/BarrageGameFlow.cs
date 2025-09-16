520b7568bbb082740c1b6dfbeac717a021e0f4f4
ayato0913a <ayato0913a@gmail.com>
2025-09-16 09:53:11 +0900

    [ add ] 連打ゲーム管理のスクリプトを作成

---
+++ Assets/Scripts/BarrageEvent/BarrageGameFlow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class BarrageGameFlow : MonoBehaviour
+{
+    [SerializeField] private BarrageGameController _barGameCon;
+    [SerializeField] private BarrageJumpController _barJumpCon;
+    [SerializeField] private ScoreManager _scoreManager;
+    [SerializeField] private Transform _jumpTarget;
+
+    private void Start()
+    {
+        _barJumpCon.OnBarrageJumpStarted += () =>
+        {
+            Debug.Log("ジャンプ開始 → 連打ゲーム開始！！");
+            _barGameCon.StartBarrage(_barJumpCon._jumpDuration);
+        };
+
+        _barJumpCon.OnBarrageJumpFinished += () =>
+        Debug.Log("ジャンプ終了");
+
+        _barGameCon.OnBarrageGameFinished += (count) =>
+        {
+            _scoreManager.AddMashScore(count);
+            // 後でリザルトかなんか追加
+        };
+    }
+
+    public void StartFinalJump()
+    {
+        _barJumpCon.StartBarrageJump(_jumpTarget.position);
+    }
+}
