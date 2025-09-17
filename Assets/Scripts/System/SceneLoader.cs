c9b703734731be943d8b8ceb433b30961b791953
ayato0913a <ayato0913a@gmail.com>
2025-09-18 03:25:56 +0900

    [ update ] Sceneをロードする際にFadeするように

--- Assets/Scripts/System/SceneLoader.cs
+++ Assets/Scripts/System/SceneLoader.cs
@@ -1,18 +1,45 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
+using DG.Tweening;
 
 /// <summary>
 ///         SceneLoadを制御するクラス
 /// </summary>
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private Image _fadeImage;
+    [SerializeField] private float _fadeTime = 1f;
+
+    private void Awake()
+    {
+        if(_fadeImage != null)
+        {
+            Color c = _fadeImage.color;
+            c.a = 0f;
+            _fadeImage.color = c;
+            _fadeImage.gameObject.SetActive(false);
+        }
+    }
+
     /// <summary>
     ///         シーンを同期ロード
     /// </summary>
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        if (_fadeImage == null)
+        {
+            Debug.LogError("FadeImage が未設定です！");
+            return;
+        }
+
+        _fadeImage.gameObject.SetActive(true);
+        _fadeImage.DOFade(1f, _fadeTime).OnComplete(() =>
+        {
+            // シーン切り替え
+            SceneManager.LoadScene(sceneName);
+        });
     }
 
     /// <summary>
