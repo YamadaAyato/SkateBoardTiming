33babdfb8c3a06138f54d0bd1b37c4dd73f47ad2
ayato0913a <ayato0913a@gmail.com>
2025-09-19 05:22:48 +0900

    [ add ] SEManager追加

---
+++ Assets/Scripts/System/SEManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SEManager : MonoBehaviour
+{
+    public static SEManager Instance { get; private set; }
+
+    [SerializeField] private AudioSource _seAudioSource;
+    [SerializeField] private AudioClip[] _seClips;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    /// <summary>
+    ///         ñºëOÇ≈éwíËÇµÇƒSEÇçƒê∂
+    /// </summary>
+    public void Play(string seName, float volume = 1f)
+    {
+        AudioClip clip = System.Array.Find(_seClips, c => c.name == seName);
+
+        _seAudioSource.PlayOneShot(clip, volume);
+    }
+}
